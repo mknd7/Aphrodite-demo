fa37f7241b5c832e8c7a05dfe341d395e033705d
Mukund Vijayaraghavan <mkndviji@gmail.com>
2018-09-30 17:27:02 +0530

    Add project boilerplate

---
+++ src/app.js
@@ -0,0 +1,3 @@
+document.write('Hello world');
+
+console.log('App loaded');
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'app-bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
